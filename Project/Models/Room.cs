285f7e469b7969b5c6dca1ad29a13085ba9bad49
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-01-30 10:40:49 -0700

    initialized

--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -3,11 +3,11 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Room : IRoom
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Item> Items { get; set; }
-        public Dictionary<string, IRoom> Exits { get; set; }
-    }
+  public class Room : IRoom
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+    public Dictionary<string, IRoom> Exits { get; set; }
+  }
 }
