82a48d3765a162e0eedf0ae6dcffbe1c190e2348
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-01-30 11:37:18 -0700

    creates items

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -10,6 +10,9 @@ namespace ConsoleAdventure.Project.Models
     //NOTE Make yo rooms here...
     public void Setup()
     {
+      //NOTE ROOMS
+      #region
+      // TODO Update descriptions
       Room Woods = new Room("The forest path", "The path into the forest is is pitch black. If you walk down it, you may get lost.");
       Room FrontPorch = new Room("Front porch", "As you walk up the stairs to the front door, the boards creak under your footsteps.");
       Room EastSide = new Room("Eastern side of the house", "Nothing of notable description.");
@@ -19,7 +22,28 @@ namespace ConsoleAdventure.Project.Models
       Room Den = new Room("Den", "Inside the second room of the house is a surprisingly well-preserved den. Inside the den is a writing desk with a single wingback chair. There is a faded painting on the western wall, the only item not in decent condition.");
       Room Basement = new Room("Basement", "With the stairs groaning under your weight with every footstep, you descend into the darkness. It is too dark to see anything.");
 
+      Woods.Exits.Add("north", FrontPorch);
+      FrontPorch.Exits.Add("north", LivingRoom);
+      FrontPorch.Exits.Add("east", EastSide);
+      FrontPorch.Exits.Add("west", WestSide);
+      EastSide.Exits.Add("north", BackOfHouse);
+      WestSide.Exits.Add("north", BackOfHouse);
+      BackOfHouse.Exits.Add("east", EastSide);
+      BackOfHouse.Exits.Add("west", WestSide);
+      LivingRoom.Exits.Add("north", Den);
+      LivingRoom.Exits.Add("south", FrontPorch);
+      LivingRoom.Exits.Add("west", Basement);
+      Den.Exits.Add("south", LivingRoom);
 
+      #endregion
+
+      //NOTE ITEMS
+      #region 
+      Item Map = new Item("Map", "While faded, the parchment appears to be a map of the woods. A dark red X on the map appears to mark the location of the house.");
+      Item Compass = new Item("Compass", "A rusted, but usable, compass.");
+      Item Lantern = new Item("Lantern", "Although the lantern appears the be very old, it seems to work properly once lit.");
+
+      #endregion
     }
   }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -10,12 +10,7 @@ namespace ConsoleAdventure.Project.Models
     public string Description { get; set; }
     public List<Item> Items { get; set; }
     public Dictionary<string, IRoom> Exits { get; set; }
-    public void AddExit(Room des)
-    {
-      Exits.Add(des.Name, des);
 
-      des.Exits.Add(Name, this);
-    }
     public Room(string name, string description)
     {
       Name = name;
