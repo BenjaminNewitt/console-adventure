453414762199c7e3da9d5b5caf311a20bcf59918
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-02 20:21:47 -0700

    more styling

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -17,8 +17,6 @@ namespace ConsoleAdventure.Project.Models
       // TODO Update descriptions
       Room Woods = new Room("The forest path", "To the north lies an abandoned structure. The path south into the forest is pitch black. If you walk down it, you will may never leave the forest.");
       Room FrontPorch = new Room("Front porch", "The boards creak under your footsteps as you step onto the porch. To the north, the front door of the house lies open.");
-      Room EastSide = new Room("Eastern side of the house", "Nothing of notice takes your interest as you travel along the eastern side of the building.");
-      Room WestSide = new Room("Western side of the house", "Traveling along the west side of the house shows nothing to be of interest.");
       Room BackOfHouse = new Room("Back of the house", "As you come around to the back of the house, you notice a single window.");
       Room LivingRoom = new Room("House Interior", "Inside of the house lies an empty room. To the west of you lies a stairwell leading downwards, and to the north is another door.");
       Room Den = new Room("Den", "Inside the second room of the house is a surprisingly well-preserved den. Inside the den is a writing desk with a single wingback chair facing the northern wall, where a single window sits. There is a faded painting on the western wall, the only item not in decent condition.");
@@ -28,16 +26,12 @@ namespace ConsoleAdventure.Project.Models
       Woods.Exits.Add("north", FrontYard);
       FrontYard.Exits.Add("north", FrontPorch);
       FrontYard.Exits.Add("south", Woods);
-      FrontYard.Exits.Add("east", EastSide);
-      FrontYard.Exits.Add("west", WestSide);
+      FrontYard.Exits.Add("east", BackOfHouse);
+      FrontYard.Exits.Add("west", BackOfHouse);
       FrontPorch.Exits.Add("north", LivingRoom);
       FrontPorch.Exits.Add("south", FrontYard);
-      EastSide.Exits.Add("north", BackOfHouse);
-      EastSide.Exits.Add("south", FrontYard);
-      WestSide.Exits.Add("north", BackOfHouse);
-      WestSide.Exits.Add("south", FrontYard);
-      BackOfHouse.Exits.Add("east", EastSide);
-      BackOfHouse.Exits.Add("west", WestSide);
+      BackOfHouse.Exits.Add("east", FrontYard);
+      BackOfHouse.Exits.Add("west", FrontYard);
       LivingRoom.Exits.Add("north", Den);
       LivingRoom.Exits.Add("south", FrontPorch);
       LivingRoom.Exits.Add("west", Basement);
