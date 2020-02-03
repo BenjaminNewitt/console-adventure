a3819be7cba5349cbe7dfd8e4615a8bacb36c2e4
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-03 11:27:06 -0700

    moves ASCII to MessageUtil.cs

---
+++ Project/Models/DeathRoom.cs
@@ -0,0 +1,11 @@
+// namespace ConsoleAdventure.Project.Models
+// {
+//   class TrapRoom : Room
+//   {
+//     public bool Disabled { get; set; }
+//     public IItem DisabledWith { get; set; }
+//     public DeathRoom(string name, string description, bool isHidden) : base(name, description, isHidden)
+//     {
+//     }
+//   }
+// }
